3acd4aec0f8e845f4a5c99203b58d8dc4a4b1004
Uwe L. Korn <uwelk@xhochy.com>
2019-02-24 21:04:51 +0100

    Correctly set CMAKE_INSTALL_LIBDIR

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,12 @@ set -e
 mkdir build-cmake
 pushd build-cmake
 
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_BUILD_TYPE=Release -GNinja ..
+cmake \
+  -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_BUILD_TYPE=Release \
+  -GNinja \
+  ..
 ninja
 ctest
 ninja install
